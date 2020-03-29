3481c0b7e3a349896e0ef37ebb4b66310422448a
Jack Procaccini <jproc1222@gmail.com>
2020-03-29 12:01:52 -0400

    Added Dr. Mitra's framework, set up project directory structure

---
+++ src/edu/brockport/treelotsales/userinterface/ViewFactory.java
@@ -0,0 +1,26 @@
+package edu.brockport.treelotsales.userinterface;
+
+import edu.brockport.treelotsales.impresario.IModel;
+
+//==============================================================================
+public class ViewFactory {
+
+	public static View createView(String viewName, IModel model)
+	{
+		return null;
+	}
+
+
+	/*
+	public static Vector createVectorView(String viewName, IModel model)
+	{
+		if(viewName.equals("SOME VIEW NAME") == true)
+		{
+			//return [A NEW VECTOR VIEW OF THAT NAME TYPE]
+		}
+		else
+			return null;
+	}
+	*/
+
+}
